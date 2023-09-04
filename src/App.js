8bfb161d05f05181df8f47e0e68c705cea9d6f77
shehnaazBanu <shehnaaz.sb052@gmail.com>
2023-09-04 20:33:39 +0530

    initial react project setup

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+// function App() {
+//   return (
+//     <div>
+//       <p>Hello World!</p>
+//     </div>
+//   );
+// }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// lets re-write  the javascript function App using arrow syntax
 
-export default App;
+  // const App = () => {
+  //   console.log("I love Mateen")
+  //   return (
+  //     <div>
+  //     <p>Hello World!</p>
+  //     </div>
+  //   );
+  // }
+
+  const Hello = () => {
+    return (
+      <div>
+        <p>Hello World!</p>
+      </div>
+    );
+  }
+
+  const App = () => {
+    return(
+      <div> 
+        <h1> Greetings</h1>
+        <Hello />
+      </div>
+    );
+  }
+
+  export default App;
